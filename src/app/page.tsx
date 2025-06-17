5b1615bb483e3582ca8ea760549fccbb600927fc
cwelly <potato3884@naver.com>
2025-06-17 16:44:04 +0900

    update test enviroments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import IntroPage from "@/components/Main/IntroPage/IntroPage";
-import Image from "next/image";
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  // 개발 환경에서만 MSW 시작
+  import("@/mocks").then(({ initMocks }) => initMocks());
+}
 
 export default function App() {
   return (
